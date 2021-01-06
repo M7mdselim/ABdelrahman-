7c66a2940966e236e0eb296c96eb2c06dbcc1c31
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2021-01-06 16:03:19 +0200

    Add files via upload

---
+++ DashboardForm/DashboardForm/ContactusForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DashboardForm
+{
+    public partial class ContactusForm : Form
+    {
+        public ContactusForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank You For ur Message :')");
+            Application.Restart();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
